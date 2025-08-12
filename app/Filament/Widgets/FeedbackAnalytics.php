647d1f9b4c321f0830733ae7adbf14f0524d7ffd
Piyush289kumar <piyushraikwar289@gmail.com>
2025-08-12 10:17:13 +0530

    Feedback & Review Module - Done

---
+++ app/Filament/Widgets/FeedbackAnalytics.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Feedback;
+use Filament\Widgets\ChartWidget;
+
+class FeedbackAnalytics extends ChartWidget
+{
+    protected static ?string $heading = 'Feedback Overview';
+
+    protected function getData(): array
+    {
+        $customerCount = Feedback::where('type', 'customer')->count();
+        $managerCount = Feedback::where('type', 'manager')->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Feedback Distribution',
+                    'data' => [$customerCount, $managerCount],
+                    'backgroundColor' => ['#4CAF50', '#FFC107'],
+                ],
+            ],
+            'labels' => ['Customer Feedback', 'Manager Feedback'],
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'pie';
+    }
+}
