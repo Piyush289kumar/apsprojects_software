a808a0edaa611c9504c90d39d261ca38ffc0d5d7
Piyush289kumar <piyushraikwar289@gmail.com>
2025-08-12 12:20:22 +0530

    add Role Permissions

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,7 @@ return new class extends Migration {
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'manager', 'staff'])->default('staff');
+            $table->string('password');            
             $table->foreignId('store_id')->nullable()->constrained()->cascadeOnDelete();
             $table->rememberToken();
             $table->timestamps();
