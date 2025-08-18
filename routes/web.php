ce3f4f54f63a2e709c6fc05a125437055258096d
Piyush289kumar <piyushraikwar289@gmail.com>
2025-08-18 11:47:43 +0530

    Redirect to /Admin Panel & Chnage Core Color Amber  to Indigo Color.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\PDFController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');    
+    return redirect('/admin');
 });
 
 Route::get('/invoice/{id}/pdf', [PDFController::class, 'generateInvoicePdf'])->name('invoice.pdf');
