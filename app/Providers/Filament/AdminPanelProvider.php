015a00d0b823e9f7726e20d997b6a9bc0c8c1890
Piyush289kumar <piyushraikwar289@gmail.com>
2025-08-23 13:35:15 +0530

    Add A Custom Widget for Company Info.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\CompanyInfoWidget;
 use App\Filament\Widgets\DashboardStats;
 use Filament\Http\Middleware\Authenticate;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
@@ -40,9 +41,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,                
+                Widgets\AccountWidget::class,
+                    // Widgets\FilamentInfoWidget::class,
+                CompanyInfoWidget::class,
                 DashboardStats::class,
-                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
